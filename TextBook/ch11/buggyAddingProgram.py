2697a61ebaed8a0433530b66964fe7976bb81fa3
itc-n23004 <n23004@std.it-college.ac.jp>
2024-06-11 01:36:26 +0000

    p305-p326 デバッグ

---
+++ TextBook/ch11/buggyAddingProgram.py
@@ -0,0 +1,9 @@
+print('足し合わせる第１の数を入力してください:')
+first = input()
+print('足し合わせる第2の数を龍力してください:')
+second = input()
+print('足し合わせる第3の数を龍力してください:')
+third = input()
+print('合計は' + first + second + third)
+
+
